a42ade55a66023f225f669d542be98cfcfde92d0
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2022-04-06 12:32:43 +0100

    Add line-by-line code explanation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ElementMaker from "./ElementMaker";
 
+// Creat an App component
 function App() {
   // Set App's state
   const [fullName, setFullName] = useState("Joe Abraham");
@@ -11,13 +12,17 @@ function App() {
       <h1>Double Click the Full Name's Value to Edit</h1>
       <div>
         <strong>Full Name: </strong>
-        {/* Invoke the ElementMaker component */}
+        {/* Invoke the ElementMaker component with some attributes */}
         <ElementMaker
-          value={fullName}
-          handleChange={(e) => setFullName(e.target.value)}
-          handleDoubleClick={() => setShowInputEle(true)}
-          handleBlur={() => setShowInputEle(false)}
-          showInputEle={showInputEle}
+          value={fullName} // Initialize the ElementMaker's value attribute with App's fullName state
+          handleChange={(e) => setFullName(e.target.value)} // Initialize the ElementMaker's handleChange attribute 
+                                                            // with a function that updates the state's fullName 
+                                                            // property with the ElementMaker's <input> element's value
+          handleDoubleClick={() => setShowInputEle(true)} // Initialize handleDoubleClick with a function that updates
+                                                          // state's showInputEle property with the Boolean value true
+          handleBlur={() => setShowInputEle(false)} // Initialize handleBlur with a function that updates state's 
+                                                    // showInputEle property with the Boolean value false
+          showInputEle={showInputEle} // Initialized showInputEle with the state's showInputEle property
         />
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Render the App component into the root DOM
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(<App />);
